31dcf528d4e45a15b660f11cddb1a57e478cd3a0
Maxence Dunnewind <maxence@dunnewind.net>
2013-02-16 09:18:34 +0100

    add notification command/periods for admins

--- icinga/manifests/contacts.pp
+++ icinga/manifests/contacts.pp
@@ -14,19 +14,29 @@
 #
 class icinga::contacts {
     @nagios_contact { 'contact_maxence':
-        ensure              => present,
-        alias               => 'maxence',
-        can_submit_commands => 1,
-        contactgroups       => 'admins',
-        email               => 'maxence.dunnewind@inria.fr',
-        register            => 1,
+        ensure                        => present,
+        alias                         => 'maxence',
+        can_submit_commands           => 1,
+        contactgroups                 => 'admins',
+        email                         => 'maxence.dunnewind@inria.fr',
+        register                      => 1,
+        service_notification_period   => '24x7',
+        host_notification_period      => '24x7',
+        service_notification_options  => 'w,u,c,r',
+        service_notification_commands => 'notify-by-email',
+        host_notification_commands    => 'host-notify-by-email'
     }
     @nagios_contact { 'contact_julien':
-        ensure              => present,
-        alias               => 'julien',
-        can_submit_commands => 1,
-        contactgroups       => 'admins',
-        email               => 'julien.lefeuvre@inria.fr',
-        register            => 1,
+        ensure                        => present,
+        alias                         => 'julien',
+        can_submit_commands           => 1,
+        contactgroups                 => 'admins',
+        email                         => 'julien.lefeuvre@inria.fr',
+        register                      => 1,
+        service_notification_period   => '24x7',
+        host_notification_period      => '24x7',
+        service_notification_options  => 'w,u,c,r',
+        service_notification_commands => 'notify-by-email',
+        host_notification_commands    => 'host-notify-by-email'
     }
 } # icinga::contacts
